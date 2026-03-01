277a5510afe12ddd9bc079ada17f9ce86bd8ba17
limecraftt <marathoncountinues@gmail.com>
2026-03-02 02:01:29 +0300

    fix: background image rendering on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+// src/pages/Home.jsx
 import React from 'react';
 import HeroSection from '../components/sections/HeroSection';
 import FeaturesSection from '../components/sections/FeaturesSection';
@@ -9,14 +10,23 @@ const Home = () => {
     <>
       {/* Continuous Hero + Features Section with University Building Background */}
       <section 
-        className="relative bg-cover bg-center bg-no-repeat"
+        className="relative bg-no-repeat"
         style={{
-          backgroundImage: 'url(/university-building.jpg)'
+          backgroundImage: 'url(/university-building.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center top',
+          backgroundAttachment: 'scroll', // 'fixed' causes zoom issues on mobile
         }}
       >
-        <HeroSection />
-        <FeaturesSection />
-        <StatsSection />
+        {/* Dark overlay to improve text readability on mobile */}
+        <div className="absolute inset-0 bg-black/30 md:bg-black/20" />
+        
+        {/* Content needs relative positioning to sit above overlay */}
+        <div className="relative z-10">
+          <HeroSection />
+          <FeaturesSection />
+          <StatsSection />
+        </div>
       </section>
 
       <VisitUsSection />
